a28a63158f8067890579ba687dd9942fac5c0bbd
Timm Schoenborn <timmschoenborn@gmail.com>
2023-12-19 11:03:29 -0600

    adds user schema and model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+/////////////////////////////////////
+//// Our Schema and dependencies ////
+/////////////////////////////////////
+const mongoose = require('../utils/connection')
+
+// destructuring the Schema and model from mongoose
+const { Schema, model } = mongoose
+
+// const someObj = {
+//     a: 'something',
+//     b: 'something else',
+//     donut: 'good'
+// }
+
+// const { donut, a, b } = someObj
+// console.log(donut)
+// console.log(a)
+// console.log(b)
+
+///////////////////////////
+//// Schema definition ////
+///////////////////////////
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+///////////////////////////
+//// create user model ////
+///////////////////////////
+const User = model('User', userSchema)
+
+///////////////////////////
+//// export user model ////
+///////////////////////////
+module.exports = User
