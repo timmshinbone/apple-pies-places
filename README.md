d5c93420bd9b8e382c076920e4ee91f86196ae24
Timm Schoenborn <timmschoenborn@gmail.com>
2023-12-19 09:18:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Apple Pies Places app
+
+An app that lets you find out more about countries around the world.
---
+++ server.js
@@ -0,0 +1,38 @@
+//////////////////////////////////
+//// Import Dependencies      ////
+//////////////////////////////////
+const express = require('express') // import express framework
+require('dotenv').config() // import/load ENV variables
+const path = require('path') // import path module
+
+/////////////////////////
+//// Import Routers  ////
+/////////////////////////
+
+////////////////////////////////
+//// Create the app object  ////
+////////////////////////////////
+const app = express() // call the express function
+
+/////////////////////
+//// Middleware  ////
+/////////////////////
+
+/////////////////
+//// Routes  ////
+/////////////////
+// basic home route
+app.get('/', (req, res) => {
+    res.send('the app is connected')
+})
+
+//////////////////////////
+//// Server Listener  ////
+//////////////////////////
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log('Your server is running, better go catch it')
+})
+
+// End
