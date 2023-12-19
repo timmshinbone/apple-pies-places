c0d4f8eca47f7fb991d35574eb46c98d78adfeb6
Timm Schoenborn <timmschoenborn@gmail.com>
2023-12-19 09:55:30 -0600

    adds middleware function

--- server.js
+++ server.js
@@ -4,19 +4,24 @@
 const express = require('express') // import express framework
 require('dotenv').config() // import/load ENV variables
 const path = require('path') // import path module
-
+const middleware = require('./utils/middleware')
 /////////////////////////
 //// Import Routers  ////
 /////////////////////////
 
-////////////////////////////////
-//// Create the app object  ////
-////////////////////////////////
+////////////////////////////////////////////////////
+//// Create the app object + set up view engine ////
+////////////////////////////////////////////////////
 const app = express() // call the express function
+
+// view engine - ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 /////////////////////
 //// Middleware  ////
 /////////////////////
+middleware(app)
 
 /////////////////
 //// Routes  ////
